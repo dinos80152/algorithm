0f5e2ca35baa68b525dd55c08666b243ab3ac92a
DinoLai <dinos80152@gmail.com>
2017-02-07 02:41:51 +0800

    add hamming distance test

---
+++ hamming_distance_test.go
@@ -0,0 +1,25 @@
+package main
+
+import "testing"
+
+func TestHammingDistance(t *testing.T) {
+	type args struct {
+		x int
+		y int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{"test", args{4, 1}, 2},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := HammingDistance(tt.args.x, tt.args.y); got != tt.want {
+				t.Errorf("HammingDistance() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
