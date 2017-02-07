682b84315a6c964af571c12e478220d63bde935d
DinoLai <dinos80152@gmail.com>
2017-02-07 12:47:44 +0800

    optimize by &

--- hamming_distance.go
+++ hamming_distance.go
@@ -1,12 +1,11 @@
 package main
 
-import (
-	"strconv"
-	"strings"
-)
-
 func HammingDistance(x int, y int) int {
 	xorInt := x ^ y
-	xorString := strconv.FormatInt(int64(xorInt), 2)
-	return strings.Count(xorString, "1")
+	var diff int
+	for xorInt > 0 {
+		diff++
+		xorInt = xorInt & (xorInt - 1)
+	}
+	return diff
 }
