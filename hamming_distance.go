cf0c6a5029e9669ae9a6c4fd2026285f355101e2
DinoLai <dinos80152@gmail.com>
2017-02-07 12:33:04 +0800

    refactor by xor

--- hamming_distance.go
+++ hamming_distance.go
@@ -6,46 +6,7 @@ import (
 )
 
 func HammingDistance(x int, y int) int {
-	xBinaryString, yBinaryString := strconv.FormatInt(int64(x), 2), strconv.FormatInt(int64(y), 2)
-	xBoolArray, yBoolArray := binaryString2BoolArray(xBinaryString), binaryString2BoolArray(yBinaryString)
-
-	var diff int
-	var longSlice, shortSlice []bool
-
-	longSlice, shortSlice = sortByArrayLength(xBoolArray, yBoolArray)
-
-	for index, value := range longSlice {
-		if value != shortSlice[index] {
-			diff++
-		}
-	}
-
-	return diff
-}
-
-func binaryString2BoolArray(binS string) []bool {
-	stringArray := strings.Split(binS, "")
-	var boolArray []bool
-	boolArray = make([]bool, len(stringArray))
-	arrLength := len(stringArray)
-	for index, value := range stringArray {
-		boolArray[arrLength-index-1], _ = strconv.ParseBool(value)
-	}
-
-	return boolArray
-}
-
-func sortByArrayLength(xBoolArray []bool, yBoolArray []bool) (longSlice []bool, shortSlice []bool) {
-	if len(xBoolArray) >= len(yBoolArray) {
-		longSlice = make([]bool, len(xBoolArray))
-		shortSlice = make([]bool, len(xBoolArray))
-		copy(longSlice, xBoolArray)
-		copy(shortSlice, yBoolArray)
-	} else {
-		longSlice = make([]bool, len(yBoolArray))
-		shortSlice = make([]bool, len(yBoolArray))
-		copy(longSlice, yBoolArray)
-		copy(shortSlice, xBoolArray)
-	}
-	return longSlice, shortSlice
+	xorInt := x ^ y
+	xorString := strconv.FormatInt(int64(xorInt), 2)
+	return strings.Count(xorString, "1")
 }
