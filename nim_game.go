18164dbda5a0f053f47088b5a369915651eeb235
DinoLai <dinos80152@gmail.com>
2017-02-08 15:50:54 +0800

    add nim game solution

---
+++ nim_game.go
@@ -0,0 +1,8 @@
+package main
+
+func CanWinNim(n int) bool {
+	if n%4 == 0 {
+		return false
+	}
+	return true
+}
---
+++ nim_game_test.go
@@ -0,0 +1,29 @@
+package main
+
+import "testing"
+
+func TestCanWinNim(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{
+			"test", args{4}, false,
+		},
+		{
+			"test2", args{11}, true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CanWinNim(tt.args.n); got != tt.want {
+				t.Errorf("CanWinNim() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
