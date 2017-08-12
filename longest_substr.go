afc1553c93686970e48eaf1c777814eca8741ca7
DinoLai <dinos80152@gmail.com>
2017-08-12 22:42:57 +0800

    add longest substring solution and test

---
+++ longest_substr.go
@@ -0,0 +1,20 @@
+package main
+
+import "bytes"
+
+func lengthOfLongestSubstring(s string) int {
+	var subStr, topStr []byte
+	for i := 0; i < len(s); i++ {
+		char := s[i]
+		index := bytes.IndexByte(subStr, char)
+		if index == -1 {
+			subStr = append(subStr, char)
+		} else {
+			subStr = append(subStr[index+1:], char)
+		}
+		if len(subStr) > len(topStr) {
+			topStr = subStr
+		}
+	}
+	return len(topStr)
+}
