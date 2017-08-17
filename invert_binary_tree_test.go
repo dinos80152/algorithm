215f182833ea7d3fe09ba4ebad7d6c41c6ccb402
DinoLai <dinos80152@gmail.com>
2017-08-17 10:31:05 +0800

    add invert binary tree solution and test

---
+++ invert_binary_tree_test.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_invertTree(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *TreeNode
+	}{
+		{"name", args{&TreeNode{
+			Val: 4,
+			Left: &TreeNode{
+				Val: 2,
+				Left: &TreeNode{
+					Val: 1,
+				},
+				Right: &TreeNode{
+					Val: 3,
+				},
+			},
+			Right: &TreeNode{
+				Val: 7,
+				Left: &TreeNode{
+					Val: 6,
+				},
+				Right: &TreeNode{
+					Val: 9,
+				},
+			},
+		}}, &TreeNode{
+			Val: 4,
+			Left: &TreeNode{
+				Val: 7,
+				Left: &TreeNode{
+					Val: 9,
+				},
+				Right: &TreeNode{
+					Val: 6,
+				},
+			},
+			Right: &TreeNode{
+				Val: 2,
+				Left: &TreeNode{
+					Val: 3,
+				},
+				Right: &TreeNode{
+					Val: 1,
+				},
+			},
+		}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := invertTree(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("invertTree() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
