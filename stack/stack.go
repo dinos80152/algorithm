c4701106574bd71ee438a3bd1b5d030c3a1dff21
DinoLai <dinos80152@gmail.com>
2019-01-08 19:52:27 +0800

    move stack to root dir

--- stack/stack.go
+++ stack/stack.go
@@ -1,6 +1,12 @@
-//评测题目: 1. integer stack, pop, push; 2. min; 3 o(1); 4. thread safety 5. non-blocking
+// 1. integer stack, pop, push;
+// 2. min;
+// 3. o(1) for pop, push, min;
+// 4. thread safety
+// 5. non-blocking
 
-package main
+// Alibaba 研发效能事业部-高级JAVA开发工程师/专家-北京/杭州 interview
+
+package stack
 
 import "fmt"
 
@@ -8,7 +14,7 @@ import "fmt"
 // {5,7,9,3,1}
 // {5,7,9,3,1,2}
 func main() {
-	s := stack{
+	s := Stack{
 		minTemp: make(map[int]int),
 	}
 	s.Push(5)
@@ -35,14 +41,14 @@ func main() {
 	fmt.Println(s)
 }
 
-type stack struct {
+type Stack struct {
 	elements []int
 	length   int
 	min      int
 	minTemp  map[int]int
 }
 
-func (s *stack) Push(element int) {
+func (s *Stack) Push(element int) {
 	s.elements = append(s.elements, element)
 	s.length++
 	if s.length == 1 || element < s.min {
@@ -51,7 +57,7 @@ func (s *stack) Push(element int) {
 	s.minTemp[s.length] = s.min
 }
 
-func (s *stack) Pop() int {
+func (s *Stack) Pop() int {
 	last := s.elements[s.length-1]
 	s.elements = s.elements[:s.length-1]
 	delete(s.minTemp, s.length)
@@ -60,6 +66,6 @@ func (s *stack) Pop() int {
 	return last
 }
 
-func (s *stack) Min() int {
+func (s *Stack) Min() int {
 	return s.minTemp[s.length]
 }
